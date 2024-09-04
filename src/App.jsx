751a8b54830ccc157299022e931fafc1ff24bbeb
Dhruv <aktedhruv@gmail.com>
2024-09-04 15:23:45 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h2 className="text-red-700 text-5xl text-center text-slate-700  ">Hello</h2>
+      <div className="min-h-10 min-w-24 p-10 bg-red-600 flex justify-center">
+          <h1 className='text-5xl text-yellow-600'>Hello</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
